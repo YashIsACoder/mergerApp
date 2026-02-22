4660683d600e8e83f5e80459138f0d94fd6c2400
Yashwanth Madan <z5481579@ad.unsw.edu.au>
2026-02-22 21:29:12 +1100

    finished merge service h file

---
+++ src/core/MergeService.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <QObject>
+#include <QStringList>
+
+class QProcess;
+
+class MergeService : public QObject
+{
+  Q_OBJECT
+
+public:
+  explicit MergeService(QObject *parent = nullptr);
+  void merge(const QStringList &inputs, const QStringList &output);
+
+signals:
+  void finished(bool done, const QString &msg);
+
+private:
+  QProcess *process = nullptr;
+};
