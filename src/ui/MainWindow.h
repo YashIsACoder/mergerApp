4c1b87fb8f267482393e7bf20066c0d5ce9b1c08
Yashwanth Madan <z5481579@ad.unsw.edu.au>
2026-02-22 20:25:33 +1100

    Finished adding member functions from sys design

--- src/ui/MainWindow.h
+++ src/ui/MainWindow.h
@@ -6,4 +6,31 @@ class QProgressBar;
 class QLabel;
 class MergeService;
 
+class MainWindow : public QMainWindow
+{
+  Q_OBJECT
+
+public:
+  explicit MainWindow(QWidget *parent = nullptr);
+
+private slots:
+  void addFiles();
+  void removeSelected();
+  void chooseOutput();
+  void merge();
+  void mergeFinished(bool accepted, const QString &msg);
+
+private:
+  QStringList currentFiles() const;
+  void setupUI();
+
+  QListWidget *fileList;
+  QPushButton *mergeBtn;
+  QProgressBar *progress;
+  QLabel *status;
+
+  QString outputPath;
+  MergeService *service;
+};
+
 
